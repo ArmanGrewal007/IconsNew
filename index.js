6566ee466c328342dd4624ebdce9eca1a9a294ba
ArmanGrewal007 <armansinghgrewal@gmail.com>
2023-11-25 21:55:00 +0530

    added /icon

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 var express = require('express');
+var morgan = require('morgan');
 
 var app = express();
+app.use(morgan('dev'));
 const port = 3000;
 
 app.get('/', (req,res)=>{
-    res.send('Hello World');
+    res.send('Give the path /icon?i="<icon_name>"');
 })
+
+app.get('/icon', async (req, res) => {
+    try {
+        res.send('waesafeds')
+    //   const response = await handleRequest(req);
+    //   res.status(response.status).send(await response.text());
+    } catch (err) {
+      res.status(500).send(err.stack);
+    }
+  });
+
+app.get('*', async (req, res) => { res.send('ERROR 404') });
 
 app.listen(port, ()=>{
     console.log('Server started on port 3000');
