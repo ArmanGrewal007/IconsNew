eee56cb627a18d75f282ed0b4ae247573c521edb
ArmanGrewal007 <armansinghgrewal@gmail.com>
2023-11-25 22:58:31 +0530

    deployed to vercel too

--- build.js
+++ build.js
@@ -6,13 +6,13 @@
 const fs = require('fs');
 
 // Synchronously read contents of icons directory, and stores list of files in iconsDir
-const iconsDir = fs.readdirSync('./icons');
+const iconsDir = fs.readdirSync('./public/icons');
 // Loop through each .svg file and populate icons object with elasticsearch-dark:SVG_CONTENT pairs
 const icons = {};
 for (const icon of iconsDir) {
   const name = icon.replace('.svg', '').toLowerCase();
-  icons[name] = String(fs.readFileSync(`./icons/${icon}`));
+  icons[name] = String(fs.readFileSync(`./public/icons/${icon}`));
 }
 // Create a dist directory if it doesn't exist, then write our data to icons.json in that directory
-if (!fs.existsSync('./dist')) fs.mkdirSync('./dist');
-fs.writeFileSync('./dist/icons.json', JSON.stringify(icons));
+if (!fs.existsSync('./public/dist')) fs.mkdirSync('./public/dist');
+fs.writeFileSync('./public/dist/icons.json', JSON.stringify(icons));
